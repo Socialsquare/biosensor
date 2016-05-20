3ce6bbe197a32cbd60f36ca680ee9af911a30121
Simon Carstensen <me@simonbc.com>
2016-05-20 13:13:37 +0200

    Add Teacher model

--- schools/models.py
+++ schools/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class School(models.Model):
     class Meta:
@@ -11,3 +12,11 @@ class School(models.Model):
     contact_email = models.CharField(max_length=100, blank=False)
     address = models.TextField(max_length=400, blank=False)
 
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    school = models.ForeignKey('School')
+    subjects = models.TextField(max_length=400, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
