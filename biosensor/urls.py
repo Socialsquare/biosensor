b04faa205593d7ee0eca435d2827339f8b8abe14
Simon Carstensen <me@simonbc.com>
2016-05-20 13:11:31 +0200

    Move home page view to biosensor app

--- biosensor/urls.py
+++ biosensor/urls.py
@@ -1,25 +1,12 @@
-"""biosensor URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
 import bioadmin
+from . import views
 
 urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
+    url(r'^schools/', include('schools.urls', namespace='schools')),
     url(r'^admin/', admin.site.urls),
     url(r'^bio-admin/', include('bioadmin.urls', namespace='bioadmin')),
-    url(r'^$', 'bioadmin.views.homepage', name='homepage'),
+    url(r'^$', views.homepage, name='homepage'),
 ]
