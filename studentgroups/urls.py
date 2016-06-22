3e71114c545a43940f16e8d50566266a48d9b625
Simon Carstensen <me@simonbc.com>
2016-06-22 12:48:18 +0200

    Add edit report feature

--- studentgroups/urls.py
+++ studentgroups/urls.py
@@ -10,5 +10,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
-    url(r'^biosensors/(\d+)/reports/new$', views.new_report, name='new_report')
+    url(r'^biosensors/(\d+)/reports/new$', views.new_report, name='new_report'),
+    url(r'^biosensors/(\d+)/reports/edit/(\d+)$', views.edit_report, name='edit_report')
 ]
--- studentgroups/views.py
+++ studentgroups/views.py
@@ -23,7 +23,6 @@ def dashboard(request):
 @student_group_required
 def new_report(request, biosensor_id):
     biosensor = get_object_or_404(Biosensor, id=biosensor_id)
-    form = ReportForm()
     if request.method == 'POST':
         form = ReportForm(request.POST)
         if form.is_valid():
@@ -36,8 +35,39 @@ def new_report(request, biosensor_id):
             messages.success(request, "Du har uplopadet din rapport")
             return redirect(biosensor)
 
+    form = ReportForm()
     context = {
             'biosensor': biosensor,
             'form': form
             }
-    return render(request, 'studentgroups/new_report.html', context)
+    return render(request, 'studentgroups/report.html', context)
+
+@login_required
+@student_group_required
+def edit_report(request, biosensor_id, report_id):
+    report = get_object_or_404(StudentReport, id=report_id)
+    if request.method == 'POST':
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            report.introduction = form.cleaned_data['introduction']
+            report.method = form.cleaned_data['method']
+            report.results = form.cleaned_data['results']
+            report.discussion = form.cleaned_data['discussion']
+            report.conclusion = form.cleaned_data['conclusion']
+            report.save()
+            messages.success(request, "Dine ændringer er gemt")
+            return redirect(report.biosensor)
+
+    form = ReportForm({
+        'introduction': report.introduction,
+        'method': report.method,
+        'results': report.results,
+        'discussion': report.discussion,
+        'conclusion': report.conclusion
+        })
+    context = {
+            'biosensor': report.biosensor,
+            'report': report,
+            'form': form
+            }
+    return render(request, 'studentgroups/report.html', context)
