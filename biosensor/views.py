c886f7f8367e371164450316f269b75eccdfdf70
Simon Carstensen <me@simonbc.com>
2016-06-28 11:09:54 +0200

    Fetch front page content from Wagtail

--- biosensor/views.py
+++ biosensor/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from content.models import ContentPage
 from biobricks.models import Category, Biobrick, Biosensor
 
 def homepage(request):
+    try:
+        frontpage = ContentPage.objects.get(slug='front')
+    except:
+        frontpage = 'Fejl: Velkomstteksten til forsiden mangler!'
     d_categories = Category.objects.filter(category_type='detector')
     r_categories = Category.objects.filter(category_type='responder')
     b_categories = Category.objects.filter(category_type='biosensor')
     context = {
+            'frontpage': frontpage,
             'd_categories': d_categories,
             'r_categories': r_categories,
             'b_categories': b_categories,
