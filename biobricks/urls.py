a41a9de9d1e65731d99ff94b847bd3369b9db552
Simon Carstensen <me@simonbc.com>
2016-06-28 11:30:56 +0200

    Remove 'katalog' from biosensor routes

--- biobricks/urls.py
+++ biobricks/urls.py
@@ -10,9 +10,9 @@ from . import views
 
 urlpatterns = [
     url(r'^(\d[a-z])$', views.show, name='show'),
-    url(r'^katalog/biosensorer/(\d+)$', views.show_biosensor, name='show_biosensor'),
-    url(r'^katalog/biosensorer/ny$', views.new_biosensor, name='new_biosensor'),
-    url(r'^katalog/biosensorer/slet/(\d+)$', views.delete_biosensor, name='delete_biosensor'),
-    url(r'^katalog/biosensorer/rediger/(\d+)$', views.edit_biosensor, name='edit_biosensor'),
-    url(r'^katalog/rapporter/(\d+)$', views.show_report, name='show_report'),
+    url(r'^biosensorer/(\d+)$', views.show_biosensor, name='show_biosensor'),
+    url(r'^biosensorer/ny$', views.new_biosensor, name='new_biosensor'),
+    url(r'^biosensorer/slet/(\d+)$', views.delete_biosensor, name='delete_biosensor'),
+    url(r'^biosensorer/rediger/(\d+)$', views.edit_biosensor, name='edit_biosensor'),
+    url(r'^rapporter/(\d+)$', views.show_report, name='show_report'),
 ]
