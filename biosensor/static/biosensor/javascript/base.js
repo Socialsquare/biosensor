0b8f80960133a664ec9e12a0f7dbb15a18b072e9
Andreas Larsen <andreas@larsenwork.com>
2016-06-29 16:37:24 +0200

    form image styling

--- biosensor/static/biosensor/javascript/base.js
+++ biosensor/static/biosensor/javascript/base.js
@@ -11,12 +11,34 @@ $('.edit-mode').on( 'click', function() {
   $(this).fadeOut();
 });
 
+// Hide image and show input on report edit pages
 $('.upload-mode').on( 'click', function(e) {
   e.preventDefault();
-  $('body').addClass('upload-mode');
-  $(this).fadeOut();
-  $(this).parent().next().fadeIn();
+  $(this).parent().next().show().trigger('click');
+  $(this).parent().hide();
 });
 
 // Initiate svg4everybody https://github.com/jonathantneal/svg4everybody
 svg4everybody();
+
+// Upload file text update
+var inputs = document.querySelectorAll( '.inputfile' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.parentElement;
+	var labelVal = label.innerHTML;
+
+	input.addEventListener( 'change', function( e )
+	{
+		// var fileName = '';
+		// if( this.files && this.files.length > 1 )
+		// 	fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		// else
+		fileName = e.target.value.split( '\\' ).pop();
+
+		if( fileName )
+			label.querySelector( 'span' ).innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
+	});
+});
