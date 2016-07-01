d2d5bed8dbe26bd64157e4f02de8a33683066843
Simon Carstensen <me@simonbc.com>
2016-07-01 11:44:36 +0200

    Fix bug on biobrick route: one or two digits

--- biobricks/urls.py
+++ biobricks/urls.py
@@ -9,7 +9,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(\d[a-z])$', views.show, name='show'),
+    url(r'^(\d{1,2}[a-z])$', views.show, name='show'),
     url(r'^biosensorer/(\d+)$', views.show_biosensor, name='show_biosensor'),
     url(r'^biosensorer/ny$', views.new_biosensor, name='new_biosensor'),
     url(r'^biosensorer/slet/(\d+)$', views.delete_biosensor, name='delete_biosensor'),
