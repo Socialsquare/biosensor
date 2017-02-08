0981ce11b52e7094b3c705b62504f127ca1b030f
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-02-08 18:19:16 +0100

    Moving environment variables on every request

--- biosensor/wsgi.py
+++ biosensor/wsgi.py
@@ -14,9 +14,12 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biosensor.settings")
 
+ENVIRON_KEYS = ['DJANGO_ENV', 'DJANGO_SECRET_KEY', 'DATABASE_URL', 'SMTP_SERVER', 'SMTP_PORT']
 
 def application(environ, start_response):
     # Moves environment variables from the apache config to the os.environ dict
-    os.environ.update(environ)
+    for k in ENVIRON_KEYS:
+        os.environ[k] = environ[k]
     application = get_wsgi_application()
-    return DjangoWhiteNoise(application)
+    return DjangoWhiteNoise(application)(environ, start_response)
+
