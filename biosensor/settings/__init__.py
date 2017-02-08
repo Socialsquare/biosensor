6bfa57953ada0292b7e518ef7932ac5ae561de30
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-02-08 16:57:52 +0100

    Loading production settings when in production

--- biosensor/settings/__init__.py
+++ biosensor/settings/__init__.py
@@ -3,7 +3,7 @@ import os
 django_env = os.getenv('DJANGO_ENV', 'development')
 
 if django_env == 'production':
-    from .staging import *
+    from .production import *
 elif django_env == 'staging':
     from .staging import *
 else:
