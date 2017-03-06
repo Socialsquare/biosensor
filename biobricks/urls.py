e50f89666bccdf6d7c454d32d3206aab34da8556
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-06 23:56:57 +0100

    Adding optional trailing slashes

--- biobricks/urls.py
+++ biobricks/urls.py
@@ -10,8 +10,8 @@ from . import views
 
 urlpatterns = [
     url(r'^(\d{1,2}[a-z])$', views.show, name='show'),
-    url(r'^biosensorer/(\d+)$', views.show_biosensor, name='show_biosensor'),
-    url(r'^biosensorer/ny$', views.new_biosensor, name='new_biosensor'),
-    url(r'^biosensorer/slet/(\d+)$', views.delete_biosensor, name='delete_biosensor'),
-    url(r'^biosensorer/rediger/(\d+)$', views.edit_biosensor, name='edit_biosensor'),
+    url(r'^biosensorer/(\d+)/?$', views.show_biosensor, name='show_biosensor'),
+    url(r'^biosensorer/ny/?$', views.new_biosensor, name='new_biosensor'),
+    url(r'^biosensorer/slet/(\d+)/?$', views.delete_biosensor, name='delete_biosensor'),
+    url(r'^biosensorer/rediger/(\d+)/?$', views.edit_biosensor, name='edit_biosensor'),
 ]
